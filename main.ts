2ce0e80e551da592b316747b321c4851f1ccd65f
fzhyzamt <fzhyzamt@163.com>
2024-05-11 01:44:40 +0800

    去重，使用ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-const axios = require('axios');
-const fs = require('fs');
+import * as fs from "fs";
+import axios from "axios";
 
-const loadNormalList = async (url) => {
+const loadNormalList = async (url: string): Promise<string> => {
     const resp = await axios.get(url);
     return resp.data;
 }
@@ -13,7 +13,16 @@ const TRACKER_LIST_URL_ARRAY = [
 ]
 Promise.all(TRACKER_LIST_URL_ARRAY.map(loadNormalList))
     .then(trackerListArray => {
-        const htmlContent = `${trackerListArray.join('\n')}`;
+        let trackerList = trackerListArray.map(s => s.split('\n')
+            .map(line => line.trim())
+            .filter(Boolean))
+            .flat(1);
+        trackerList = trackerList.filter((item, index, self) => {
+            return index === self.indexOf(item);
+        });
+        console.log('tracker size: ', trackerList.length);
+
+        const htmlContent = `${trackerList.join('\r\n')}`;
         if (!fs.existsSync('dist')) {
             fs.mkdirSync('dist');
         }
